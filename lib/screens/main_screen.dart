70cce0f0e4ab3276bbf0185b505f30d828a1155f
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-06 01:31:53 +0530

    Implement MainScreen with bottom navigation and page routing

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,3 +1,9 @@
+import 'package:expenz_app/constant/colors.dart';
+import 'package:expenz_app/screens/add_new_screen.dart';
+import 'package:expenz_app/screens/budget_screen.dart';
+import 'package:expenz_app/screens/home_screen.dart';
+import 'package:expenz_app/screens/profile_screen.dart';
+import 'package:expenz_app/screens/transactions_screen.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -8,8 +14,60 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  int _currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Main Screen")));
+    final List<Widget> pages = [
+      HomeScreen(),
+      TransactionScreen(),
+      AddNewScreen(),
+      BudgetScreen(),
+      ProfileScreen(),
+    ];
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: kWhite,
+        selectedItemColor: kMainColor,
+        unselectedItemColor: kGrey,
+        selectedLabelStyle: TextStyle(
+          fontSize: 12,
+          fontWeight: FontWeight.w600,
+        ),
+        currentIndex: _currentPageIndex,
+        onTap: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        items: [
+          const BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.list_rounded),
+            label: "Transactions",
+          ),
+          BottomNavigationBarItem(
+            icon: Container(
+              decoration: BoxDecoration(
+                color: kMainColor,
+                shape: BoxShape.circle,
+              ),
+              padding: EdgeInsets.all(10),
+              child: Icon(Icons.add, color: kWhite, size: 32),
+            ),
+            label: "",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.rocket),
+            label: "Budget",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: "Profile",
+          ),
+        ],
+      ),
+      body: pages[_currentPageIndex],
+    );
   }
 }
