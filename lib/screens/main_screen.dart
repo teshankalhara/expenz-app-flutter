66744d8b9e09aac827520767a1c562fbe28d3da7
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-07 21:04:19 +0530

    Refactor MainScreen to integrate expense and income management; implement fetch and add functionalities for expenses and incomes

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,9 +1,13 @@
 import 'package:expenz_app/constant/colors.dart';
+import 'package:expenz_app/models/expence_model.dart';
+import 'package:expenz_app/models/income_model.dart';
 import 'package:expenz_app/screens/add_new_screen.dart';
 import 'package:expenz_app/screens/budget_screen.dart';
 import 'package:expenz_app/screens/home_screen.dart';
 import 'package:expenz_app/screens/profile_screen.dart';
 import 'package:expenz_app/screens/transactions_screen.dart';
+import 'package:expenz_app/services/expense_service.dart';
+import 'package:expenz_app/services/income_services.dart';
 import 'package:flutter/material.dart';
 
 class MainScreen extends StatefulWidget {
@@ -15,10 +19,49 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _currentPageIndex = 0;
+
+  List<Expense> expenseList = [];
+  List<Income> incomeList = [];
+
+  void fetchExpense() async {
+    List<Expense> loadedExpense = await ExpenseService().loadExpenses();
+    setState(() {
+      expenseList = loadedExpense;
+    });
+  }
+
+  void fetchIncome() async {
+    List<Income> loadedIncome = await IncomeServices().loadIncomes();
+    setState(() {
+      incomeList = loadedIncome;
+    });
+  }
+
+  void addNewExpense(Expense newExpense) {
+    ExpenseService().saveExpenses(newExpense, context);
+    setState(() {
+      expenseList.add(newExpense);
+    });
+  }
+
+  void addNewIncome(Income newIncome) {
+    IncomeServices().saveIncome(newIncome, context);
+    setState(() {
+      incomeList.add(newIncome);
+    });
+  }
+
+  @override
+  void initState() {
+    fetchExpense();
+    fetchIncome();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     final List<Widget> pages = [
-      AddNewScreen(),
+      AddNewScreen(addExpense: addNewExpense, addIcome: addNewIncome),
       HomeScreen(),
       TransactionScreen(),
       BudgetScreen(),
