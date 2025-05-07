f2c429a9c8874aab0bd2c26068673b99fe8d2aa6
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-07 16:29:11 +0530

    Implement AddNewScreen layout with expense and income selection

--- lib/screens/add_new_screen.dart
+++ lib/screens/add_new_screen.dart
@@ -1,3 +1,5 @@
+import 'package:expenz_app/constant/colors.dart';
+import 'package:expenz_app/constant/constants.dart';
 import 'package:flutter/material.dart';
 
 class AddNewScreen extends StatefulWidget {
@@ -8,8 +10,93 @@ class AddNewScreen extends StatefulWidget {
 }
 
 class _AddNewScreenState extends State<AddNewScreen> {
+  //state to track the expense or income
+  int _selectedMethode = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Add New Screen")));
+    return Scaffold(
+      backgroundColor: _selectedMethode == 0 ? kRed : kGreen,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: kDefalutPadding),
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: kDefalutPadding),
+              child: Stack(
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.06,
+                    decoration: BoxDecoration(
+                      color: kWhite,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        GestureDetector(
+                          onTap:
+                              () => setState(() {
+                                _selectedMethode = 0;
+                              }),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: _selectedMethode == 0 ? kRed : kWhite,
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 60,
+                              ),
+                              child: Text(
+                                "Expense",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      _selectedMethode == 0 ? kWhite : kBlack,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap:
+                              () => setState(() {
+                                _selectedMethode = 1;
+                              }),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: _selectedMethode == 1 ? kGreen : kWhite,
+                              borderRadius: BorderRadius.circular(100),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 60,
+                              ),
+                              child: Text(
+                                "Income",
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color:
+                                      _selectedMethode == 1 ? kWhite : kBlack,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
