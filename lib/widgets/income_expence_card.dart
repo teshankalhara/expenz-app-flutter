10bcc6d92d7e1c18da5bad7e139642ea5bee698f
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-07 15:44:31 +0530

    Refactor IncomeExpenceCard to include title, amount, imageUrl, and bgcolor properties with updated layout

--- lib/widgets/income_expence_card.dart
+++ lib/widgets/income_expence_card.dart
@@ -1,7 +1,20 @@
+import 'package:expenz_app/constant/colors.dart';
+import 'package:expenz_app/constant/constants.dart';
 import 'package:flutter/material.dart';
 
 class IncomeExpenceCard extends StatefulWidget {
-  const IncomeExpenceCard({super.key});
+  final String title;
+  final double amount;
+  final String imageUrl;
+  final Color bgcolor;
+
+  const IncomeExpenceCard({
+    super.key,
+    required this.title,
+    required this.amount,
+    required this.imageUrl,
+    required this.bgcolor,
+  });
 
   @override
   State<IncomeExpenceCard> createState() => _IncomeExpenceCardState();
@@ -10,6 +23,56 @@ class IncomeExpenceCard extends StatefulWidget {
 class _IncomeExpenceCardState extends State<IncomeExpenceCard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.45,
+      height: MediaQuery.of(context).size.height * 0.11,
+      decoration: BoxDecoration(
+        color: widget.bgcolor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(kDefalutPadding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.15,
+              width: MediaQuery.of(context).size.width * 0.15,
+              decoration: BoxDecoration(
+                color: kWhite,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: Image.asset(widget.imageUrl, height: 60, width: 60),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  widget.title,
+                  style: const TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color: kWhite,
+                  ),
+                ),
+                Text(
+                  "\$${widget.amount.toStringAsFixed(0)}",
+                  style: const TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: kWhite,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
