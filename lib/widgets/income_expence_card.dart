58677bfdabf9e385375d219d23e40021da6a6f16
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-07 15:25:22 +0530

    Add IncomeExpenceCard widget with initial placeholder implementation

---
+++ lib/widgets/income_expence_card.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class IncomeExpenceCard extends StatefulWidget {
+  const IncomeExpenceCard({super.key});
+
+  @override
+  State<IncomeExpenceCard> createState() => _IncomeExpenceCardState();
+}
+
+class _IncomeExpenceCardState extends State<IncomeExpenceCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
