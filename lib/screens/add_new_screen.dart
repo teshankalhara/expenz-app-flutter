c455206d7b65751c204389b7785fd6938e2dcfc2
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-07 17:49:46 +0530

    Refactor AddNewScreen layout for improved readability and structure

--- lib/screens/add_new_screen.dart
+++ lib/screens/add_new_screen.dart
@@ -1,5 +1,7 @@
 import 'package:expenz_app/constant/colors.dart';
 import 'package:expenz_app/constant/constants.dart';
+import 'package:expenz_app/models/expence_model.dart';
+import 'package:expenz_app/models/income_model.dart';
 import 'package:flutter/material.dart';
 
 class AddNewScreen extends StatefulWidget {
@@ -13,19 +15,26 @@ class _AddNewScreenState extends State<AddNewScreen> {
   //state to track the expense or income
   int _selectedMethode = 0;
 
+  ExpenseCategory _expenseCategory = ExpenseCategory.health;
+  IncomeCategory _incomeCategory = IncomeCategory.salary;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: _selectedMethode == 0 ? kRed : kGreen,
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: kDefalutPadding),
-          child: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: kDefalutPadding),
-              child: Stack(
-                children: [
-                  Container(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: kDefalutPadding),
+            child: Stack(
+              children: [
+                //tabs
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: kDefalutPadding,
+                  ),
+                  child: Container(
+                    //expense and income tabs
                     height: MediaQuery.of(context).size.height * 0.06,
                     decoration: BoxDecoration(
                       color: kWhite,
@@ -91,8 +100,111 @@ class _AddNewScreenState extends State<AddNewScreen> {
                       ],
                     ),
                   ),
-                ],
-              ),
+                ),
+                //amount filed
+                Container(
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: kDefalutPadding,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "How Much?",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.w600,
+                          // ignore: deprecated_member_use
+                          color: kLightGrey.withOpacity(0.8),
+                        ),
+                      ),
+                      TextField(
+                        style: TextStyle(
+                          fontSize: 60,
+                          color: kWhite,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "0",
+                          hintStyle: TextStyle(
+                            fontSize: 60,
+                            // ignore: deprecated_member_use
+                            color: kWhite,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //user data form
+                Container(
+                  height: 300,
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.30,
+                  ),
+                  decoration: BoxDecoration(
+                    color: kWhite,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: kDefalutPadding,
+                      vertical: 20,
+                    ),
+                    child: Form(
+                      child: Column(
+                        children: [
+                          DropdownButtonFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: kDefalutPadding,
+                              ),
+                            ),
+                            items:
+                                _selectedMethode == 0
+                                    ? ExpenseCategory.values.map((category) {
+                                      return DropdownMenuItem(
+                                        value: category,
+                                        child: Text(category.name),
+                                      );
+                                    }).toList()
+                                    : IncomeCategory.values.map((category) {
+                                      return DropdownMenuItem(
+                                        value: category,
+                                        child: Text(category.name),
+                                      );
+                                    }).toList(),
+                            value:
+                                _selectedMethode == 0
+                                    ? _expenseCategory
+                                    : _incomeCategory,
+                            onChanged: (value) {
+                              setState(() {
+                                _selectedMethode == 0
+                                    ? _expenseCategory =
+                                        value as ExpenseCategory
+                                    : _incomeCategory = value as IncomeCategory;
+                              });
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
