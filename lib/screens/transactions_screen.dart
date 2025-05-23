04f57c49bd50afe24eeb7dcc7c553eecfc944f70
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-23 23:20:00 +0530

    Implement TransactionScreen to display a list of expenses with Dismissible functionality

--- lib/screens/transactions_screen.dart
+++ lib/screens/transactions_screen.dart
@@ -1,7 +1,13 @@
+import 'package:expenz_app/constant/colors.dart';
+import 'package:expenz_app/constant/constants.dart';
+import 'package:expenz_app/models/expence_model.dart';
+import 'package:expenz_app/widgets/expense_card.dart';
 import 'package:flutter/material.dart';
 
 class TransactionScreen extends StatefulWidget {
-  const TransactionScreen({super.key});
+  final List<Expense> expenseList;
+
+  const TransactionScreen({super.key, required this.expenseList});
 
   @override
   State<TransactionScreen> createState() => _TransactionScreenState();
@@ -10,6 +16,67 @@ class TransactionScreen extends StatefulWidget {
 class _TransactionScreenState extends State<TransactionScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Transaction Screen")));
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(kDefalutPadding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "See your financial report",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: kMainColor,
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              const Text(
+                "Expenses",
+                style: TextStyle(
+                  fontSize: 19,
+                  fontWeight: FontWeight.bold,
+                  color: kBlack,
+                ),
+              ),
+              const SizedBox(height: 20),
+
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.35,
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      ListView.builder(
+                        shrinkWrap: true,
+                        scrollDirection: Axis.vertical,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemCount: widget.expenseList.length,
+                        itemBuilder: (context, index) {
+                          final expense = widget.expenseList[index];
+                          return Dismissible(
+                            key: ValueKey(expense),
+                            direction: DismissDirection.startToEnd,
+                            child: ExpenseCard(
+                              title: expense.title,
+                              date: expense.date,
+                              amount: expense.amount,
+                              category: expense.category,
+                              description: expense.description,
+                              createdAt: expense.time,
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
