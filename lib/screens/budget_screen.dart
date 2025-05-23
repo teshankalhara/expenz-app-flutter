d872f84e44a2827a509562a999e923f24a6a0ee0
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-05-24 01:30:50 +0530

    Add BudgetScreen with category selection and visualization for expenses and income

--- lib/screens/budget_screen.dart
+++ lib/screens/budget_screen.dart
@@ -1,15 +1,167 @@
+import 'package:expenz_app/constant/colors.dart';
+import 'package:expenz_app/constant/constants.dart';
+import 'package:expenz_app/models/expence_model.dart';
+import 'package:expenz_app/models/income_model.dart';
+import 'package:expenz_app/widgets/category_card.dart';
+import 'package:expenz_app/widgets/pie_chart.dart';
 import 'package:flutter/material.dart';
 
 class BudgetScreen extends StatefulWidget {
-  const BudgetScreen({super.key});
+  final Map<ExpenseCategory, double> expenseCategoryTotals;
+  final Map<IncomeCategory, double> incomeCategoryTotals;
+
+  const BudgetScreen({
+    super.key,
+    required this.expenseCategoryTotals,
+    required this.incomeCategoryTotals,
+  });
 
   @override
   State<BudgetScreen> createState() => _BudgetScreenState();
 }
 
 class _BudgetScreenState extends State<BudgetScreen> {
+  int _selected = 0;
+
+  Color getCategoryColor(dynamic category) {
+    if (category is ExpenseCategory) {
+      return expenseCategoryColors[category]!;
+    } else {
+      return incomeCategoryColors[category]!;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Budget Screen")));
+    final data =
+        _selected == 0
+            ? widget.expenseCategoryTotals
+            : widget.incomeCategoryTotals;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "Financial Report",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            color: kBlack,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: kDefalutPadding,
+                  vertical: kDefalutPadding / 2,
+                ),
+                child: Container(
+                  height: MediaQuery.of(context).size.height * 0.06,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(100),
+                    color: kWhite,
+                    boxShadow: [
+                      BoxShadow(color: kBlack.withOpacity(0.1), blurRadius: 20),
+                    ],
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selected = 0;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: _selected == 1 ? kWhite : kRed,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 60,
+                            ),
+                            child: Text(
+                              "Expense",
+                              style: TextStyle(
+                                color: _selected == 0 ? kWhite : kBlack,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            _selected = 1;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(100),
+                            color: _selected == 0 ? kWhite : kGreen,
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 10,
+                              horizontal: 60,
+                            ),
+                            child: Text(
+                              "Income",
+                              style: TextStyle(
+                                color: _selected == 1 ? kWhite : kBlack,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              // Pie chart
+              Chart(
+                expenseCategoryTotals: widget.expenseCategoryTotals,
+                incomeCategoryTotals: widget.incomeCategoryTotals,
+                isExpense: _selected == 0,
+              ),
+              const SizedBox(height: 30),
+              // List of categories
+              Column(
+                children: [
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: data.length,
+                    itemBuilder: (context, index) {
+                      final category = data.keys.toList()[index];
+                      final total = data.values.toList()[index];
+                      return CategoryCard(
+                        title: category.name,
+                        amount: total,
+                        totalAmount: data.values.reduce((a, b) => a + b),
+                        progressColor: getCategoryColor(category),
+                        isExpense: _selected == 0,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
